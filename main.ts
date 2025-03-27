ba4ed7f07c7a06b354a64c30410c421e18368c62
Amitron-Dev <martingrellier79@gmail.com>
2025-03-27 18:35:35 +0100

    Attente test (no time set)

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-	
+    Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
+    for (let index = 0; index < 18; index++) {
+        if (Kitronik_Move_Motor.measure() <= 1) {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
+            basic.pause(700)
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+            basic.pause(200)
+        } else {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
+        }
+    }
 })
 basic.pause(1000)
 basic.showString("23")
