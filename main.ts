1fe72b2356ccb89fdbf1adcee9836847807520cf
Amitron-Dev <martingrellier79@gmail.com>
2025-03-28 13:23:14 +0100

    patch-1

--- main.ts
+++ main.ts
@@ -1,20 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
-    for (let index = 0; index < 18; index++) {
-        if (Kitronik_Move_Motor.measure() <= 1) {
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
-            basic.pause(700)
-            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
-            basic.pause(200)
-        } else {
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
-        }
-    }
+    activate = 1
 })
+let activate = 0
 basic.pause(1000)
 basic.showString("23")
 basic.forever(function () {
-	
+    if (activate == 1) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 26)
+        if (Kitronik_Move_Motor.measure() < 6) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 26)
+            basic.pause(1000)
+            Kitronik_Move_Motor.stop()
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 10)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 26)
+            basic.pause(500)
+        }
+    }
 })
