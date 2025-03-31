084f2241cb973782c3e101f91f3b977876e2abd5
Amitron-Dev <martingrellier79@gmail.com>
2025-03-31 09:15:46 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
-    for (let index = 0; index < 18; index++) {
-        if (Kitronik_Move_Motor.measure() <= 1) {
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
-            basic.pause(700)
-            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
-            basic.pause(200)
-        } else {
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, 45)
-        }
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 50)
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 50)
+    basic.pause(200)
+    if (0 <= 0) {
+    	
+    } else {
+    	
     }
 })
+let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
 basic.pause(1000)
 basic.showString("23")
 basic.forever(function () {
-	
+    moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(2)
+    moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Violet))
 })
